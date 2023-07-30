64db8c9150ab37974677a5bcb7bcd5a6169dbc99
JohnLee <lijiang01001@163.com>
2023-07-30 09:51:30 +0800

    换脸UI初始化

--- Assets/Scripts/UI/UICharacterCreat.cs
+++ Assets/Scripts/UI/UICharacterCreat.cs
@@ -4,26 +4,44 @@ using UnityEngine.UI;
 
 public class UICharacterCreat : UIBase
 {
-    /*public Button LoginBtn;
-    public TMP_InputField UIDInputField;
-    public TMP_InputField pwdInputField;
-    public TMP_Text DebugText;*/
+    public Button ApplyBtn;
+    public Button RandomBtn;
+    public TMP_InputField NameInputField;
+
+    public Button ColorItemRed;
+    public Button ColorItemBlue;
+    public Button ColorItemYellow;
+    public Button ColorItemBlack;
+
+    public Button EyeItem1;
+    public Button EyeItem2;
+    public Button EyeItem3;
+    public Button EyeItem4;
+    public Button EyeItem5;
 
     public UICharacterCreat() : base("UICharacterCreat", EUILayerType.Normal, true) { }
 
     private void OnClickLoginBtn()
     {
+
     }
 
     protected override void OnInit()
     {
-        /*LoginBtn = uiGameObject.transform.Find("LoginWindow/LoginBtn").GetComponent<Button>();
-        UIDInputField = uiGameObject.transform.Find("LoginWindow/UID").GetComponent<TMP_InputField>();
-        pwdInputField = uiGameObject.transform.Find("LoginWindow/Pwd").GetComponent<TMP_InputField>();
-        DebugText = uiGameObject.transform.Find("Debug").GetComponent<TMP_Text>();
+        ApplyBtn = uiGameObject.transform.Find("CreatWindow/ApplyBtn").GetComponent<Button>();
+        RandomBtn = uiGameObject.transform.Find("CreatWindow/RandomBtn").GetComponent<Button>();
+        NameInputField = uiGameObject.transform.Find("CreatWindow/NameBar/NameInputField").GetComponent<TMP_InputField>();
 
-        LoginBtn.onClick.RemoveAllListeners();
-        LoginBtn.onClick.AddListener(OnClickLoginBtn);*/
+        ColorItemRed = uiGameObject.transform.Find("CreatWindow/ColorBar/ColorItemRed").GetComponent<Button>();
+        ColorItemBlue = uiGameObject.transform.Find("CreatWindow/ColorBar/ColorItemBlue").GetComponent<Button>();
+        ColorItemYellow = uiGameObject.transform.Find("CreatWindow/ColorBar/ColorItemYellow").GetComponent<Button>();
+        ColorItemBlack = uiGameObject.transform.Find("CreatWindow/ColorBar/ColorItemBlack").GetComponent<Button>();
+
+        EyeItem1 = uiGameObject.transform.Find("CreatWindow/EyeBar/EyeItem1").GetComponent<Button>();
+        EyeItem2 = uiGameObject.transform.Find("CreatWindow/EyeBar/EyeItem2").GetComponent<Button>();
+        EyeItem3 = uiGameObject.transform.Find("CreatWindow/EyeBar/EyeItem3").GetComponent<Button>();
+        EyeItem4 = uiGameObject.transform.Find("CreatWindow/EyeBar/EyeItem4").GetComponent<Button>();
+        EyeItem5 = uiGameObject.transform.Find("CreatWindow/EyeBar/EyeItem5").GetComponent<Button>();
     }
 
     protected override void OnActive()
