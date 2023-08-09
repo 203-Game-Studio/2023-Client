27b5a19a7c8dae69ec230f5a17d98258f5246de9
JohnLee <lijiang01001@163.com>
2023-08-09 23:17:29 +0800

    树干shader提交

--- Assets/Shaders/VegetationTrunk.hlsl
+++ Assets/Shaders/VegetationTrunk.hlsl
@@ -1,5 +1,5 @@
-#ifndef VEGETATION_INCLUDED
-#define VEGETATION_INCLUDED
+#ifndef VEGETATION_TRUNK_INCLUDED
+#define VEGETATION_TRUNK_INCLUDED
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
@@ -24,30 +24,15 @@ struct LitVaryings
 	float2 uv           : TEXCOORD0;
     float3 positionWS   : TEXCOORD1;
 	float3 normalWS     : TEXCOORD2;
-    float4 screenPos    : TEXCOORD3;
+	float4 tangentWS     : TEXCOORD3;
+    float4 screenPos    : TEXCOORD4;
 };
 
 CBUFFER_START(UnityPerMaterial)
     float4 _BaseMap_ST;
     float4 _BaseColor;
+	float  _NormalStrength;
 	float  _HidePower;
-
-	float4 _GradientColor;
-	float  _GradientFalloff;
-	float  _GradientPosition;
-
-	float4 _ColorVariation;
-	float  _ColorVariationPower;
-	float  _NoiseScale;
-
-	float  _Scattering;
-	float  _Cutoff;
-    float  _TransStrength;
-	float  _TransNormal;
-	float  _TransScattering;
-	float  _TransDirect;
-	float  _TransAmbient;
-	float  _TransShadow;
 
     float  _WindTrunkContrast;
 	float  _WindTrunkPosition;
@@ -68,23 +53,8 @@ float _MicroPower;
 TEXTURE2D(_BaseMap);
 SAMPLER(sampler_BaseMap);
 
-TEXTURE2D(_ColorVariationNoise);
-SAMPLER(sampler_ColorVariationNoise);
-
-inline float Dither8x8Bayer(int x, int y)
-{
-	const float dither[ 64 ] = {
-        1, 49, 13, 61,  4, 52, 16, 64,
-        33, 17, 45, 29, 36, 20, 48, 32,
-        9, 57,  5, 53, 12, 60,  8, 56,
-        41, 25, 37, 21, 44, 28, 40, 24,
-        3, 51, 15, 63,  2, 50, 14, 62,
-        35, 19, 47, 31, 34, 18, 46, 30,
-        11, 59,  7, 55, 10, 58,  6, 54,
-        43, 27, 39, 23, 42, 26, 38, 22};
-	int r = y * 8 + x;
-	return dither[r] / 64;
-}
+TEXTURE2D(_BumpMap);
+SAMPLER(sampler_BumpMap);
 
 float3 mod2D289(float3 x) 
 { 
@@ -99,6 +69,21 @@ float2 mod2D289(float2 x)
 float3 permute(float3 x) 
 {
     return mod2D289(((x * 34.0) + 1.0) * x); 
+}
+
+inline float Dither8x8Bayer(int x, int y)
+{
+	const float dither[ 64 ] = {
+        1, 49, 13, 61,  4, 52, 16, 64,
+        33, 17, 45, 29, 36, 20, 48, 32,
+        9, 57,  5, 53, 12, 60,  8, 56,
+        41, 25, 37, 21, 44, 28, 40, 24,
+        3, 51, 15, 63,  2, 50, 14, 62,
+        35, 19, 47, 31, 34, 18, 46, 30,
+        11, 59,  7, 55, 10, 58,  6, 54,
+        43, 27, 39, 23, 42, 26, 38, 22};
+	int r = y * 8 + x;
+	return dither[r] / 64;
 }
 
 float snoise(float2 v)
@@ -147,20 +132,17 @@ LitVaryings LitPassVertex(LitAttributes input)
     float3 baseWindWorldPos = float3(((sin(windSpeed) * prelinNoiseVal) * posTarget).r, 0.0, ((cos(windSpeed) * (prelinNoiseVal * 0.5)) * posTarget).r);
     float4 baseWind = mul(GetWorldToObjectMatrix(),float4(baseWindWorldPos, 0.0)) * _WindMultiplier;
 
-    //微风
-    prelinNoiseVal = snoise(_Time.y * _MicroSpeed.xx + worldPos.xz);
-    prelinNoiseVal = prelinNoiseVal * 0.5 + 0.5;
-    float3 microWindFactor = clamp(sin((_MicroFrequency * (worldPos + prelinNoiseVal))), float3(-1,-1,-1), float3(1,1,1));
-    float3 microWind = microWindFactor * input.normalOS * _MicroPower * input.color.r * _MicroWindMultiplier;
-
     //将偏移加到局部空间坐标上
-    input.positionOS = input.positionOS + baseWind + float4(microWind, 0.0);
+    input.positionOS = input.positionOS + baseWind;
 
     output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
 	output.positionWS = TransformObjectToWorld(input.positionOS);
 	output.positionCS = TransformWorldToHClip(output.positionWS);
-	output.normalWS = TransformObjectToWorldNormal(input.normalOS);
     output.screenPos = ComputeScreenPos(output.positionCS);
+
+	output.normalWS = TransformObjectToWorldNormal(input.normalOS);
+	output.tangentWS.xyz = TransformObjectToWorldDir(input.tangentOS);
+    output.tangentWS.w = input.tangentOS.w;
 
     return output;
 }
@@ -168,34 +150,24 @@ LitVaryings LitPassVertex(LitAttributes input)
 half4 LitPassFragment(LitVaryings input) : SV_Target
 {
     float4 albedo = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.uv);
-
 	float3 viewDirectionWS = normalize(_WorldSpaceCameraPos.xyz  - input.positionWS);
+
+    half sign = input.tangentWS.w * unity_WorldTransformParams.w;
+    half3 bitangentWS = cross(input.normalWS, input.tangentWS) * sign;
+    half3x3 TBN = half3x3(input.tangentWS.xyz, bitangentWS, input.normalWS);
+    float3 normalVal = UnpackNormalScale(SAMPLE_TEXTURE2D(_BumpMap, sampler_BumpMap, input.uv), _NormalStrength);
+    half3 normalWS = TransformTangentToWorld(normalVal, TBN);
 
     //渐隐和采样去除生硬的边
     float4 screenPosNorm = input.screenPos / input.screenPos.w;
 	screenPosNorm.z = (UNITY_NEAR_CLIP_VALUE >= 0) ? screenPosNorm.z : screenPosNorm.z * 0.5 + 0.5;
 	float2 clipScreen = screenPosNorm.xy * _ScreenParams.xy;
 	float ditherVal = Dither8x8Bayer(fmod(clipScreen.x, 8), fmod(clipScreen.y, 8));
-	float3 ditherNormal = normalize(cross(ddy(input.positionWS), ddx(input.positionWS)));
-	float vDotD = abs(dot(viewDirectionWS, ditherNormal));
-	float ditherResult = saturate((albedo.a * (vDotD * 2.0 - 1.0) * _HidePower));
+	float ditherResult = saturate((albedo.a * _HidePower));
 	ditherVal = step(ditherVal, ditherResult);
 
     //clip
     clip(ditherVal - 0.25);
-
-    float3 normalWS = normalize(input.normalWS);
-
-    //梯度颜色插值
-    float gradientLerpVal = saturate(((-1.0 - normalWS.y + _GradientPosition * 3.0) / _GradientFalloff));
-    float4 gradientLerpColor = lerp(_BaseColor , _GradientColor , gradientLerpVal);
-
-    //世界uv 模拟云
-    float4 cloudLerpVal = saturate(lerp(_ColorVariation, _ColorVariation / max(1.0 - gradientLerpColor, 0.00001), _ColorVariationPower));
-    float4 noiseVal = max(SAMPLE_TEXTURE2D(_ColorVariationNoise, sampler_ColorVariationNoise, float2(input.positionWS.x, input.positionWS.z) * _NoiseScale), 0.0001);
-    float4 cloudVal = lerp(gradientLerpColor, cloudLerpVal,  _ColorVariationPower * pow(noiseVal, 3));
-
-    albedo *= cloudVal;
 
     //GI
     float3 SH = SampleSH(normalWS);
@@ -215,21 +187,11 @@ half4 LitPassFragment(LitVaryings input) : SV_Target
     surfaceData.normalTS = half3(0, 0, 1);
     surfaceData.emission = 0;
     surfaceData.occlusion = 1;
-    surfaceData.alpha = albedo.a;
+    surfaceData.alpha = 1;
     surfaceData.clearCoatMask = 0;
     surfaceData.clearCoatSmoothness = 1;
 
-    //先用URP PBR计算 后面可以改成Lambert +（菲涅尔+GGX）/（或者NdotH替代）
     half4 color = UniversalFragmentPBR(inputData, surfaceData);
-
-    //次表面散射
-    Light mainLight = GetMainLight(inputData.shadowCoord);
-	float3 mainAtten = mainLight.color * mainLight.distanceAttenuation;
-    mainAtten = lerp(mainAtten, mainAtten * mainLight.shadowAttenuation, _TransShadow);
-    half3 mainLightDir = mainLight.direction + normalWS * _TransNormal;
-    half mainLightVdotL = pow(saturate(dot(viewDirectionWS, -mainLightDir)), _TransScattering);
-    half3 mainLightTranslucency = mainAtten * (mainLightVdotL * _TransDirect + inputData.bakedGI * _TransAmbient) * _Scattering.xxx;
-    color.rgb += albedo * mainLightTranslucency * _TransStrength;
 
     return color;
 }
