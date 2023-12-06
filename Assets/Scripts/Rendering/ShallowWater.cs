f9d01812a27f8d53de8aa22c093ca2d63d7b5b21
JohnLee <lijiang01001@163.com>
2023-12-07 00:04:54 +0800

    浅水初始化

---
+++ Assets/Scripts/Rendering/ShallowWater.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ShallowWater : MonoBehaviour
+{
+    [SerializeField] private Material waterMat;
+
+    private RenderTexture h1RT;
+
+    private void Awake()
+    {
+        h1RT = CreateRT(1024);
+        waterMat.SetTexture("_H1RT", h1RT);
+    }
+
+    private Mesh _waterMesh;
+    private Mesh waterMesh{
+        get{
+            if(!_waterMesh){
+                _waterMesh = CreatePlaneMesh(256);
+            }
+            return _waterMesh;
+        }
+    }
+
+    Mesh CreatePlaneMesh(int size){
+        var mesh = new Mesh();
+
+        var sizePerGrid = 0.5f;
+        var totalMeterSize = size * sizePerGrid;
+        var gridCount = size * size;
+        var triangleCount = gridCount * 2;
+
+        var vOffset = - totalMeterSize * 0.5f;
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        float uvStrip = 1f / size;
+        for(var z = 0; z <= size;z ++){
+            for(var x = 0; x <= size; x ++){
+                vertices.Add(new Vector3(vOffset + x * 0.5f,0,vOffset + z * 0.5f));
+                uvs.Add(new Vector2(x * uvStrip,z * uvStrip));
+            }
+        }
+        mesh.SetVertices(vertices);
+        mesh.SetUVs(0,uvs);
+
+        int[] indices = new int[triangleCount * 3];
+
+        for(var gridIndex = 0; gridIndex < gridCount ; gridIndex ++){
+            var offset = gridIndex * 6;
+            var vIndex = (gridIndex / size) * (size + 1) + (gridIndex % size);
+
+            indices[offset] = vIndex;
+            indices[offset + 1] = vIndex + size + 1;
+            indices[offset + 2] = vIndex + 1;
+            indices[offset + 3] = vIndex + 1; 
+            indices[offset + 4] = vIndex + size + 1;
+            indices[offset + 5] = vIndex + size + 2;
+        }
+        mesh.SetIndices(indices,MeshTopology.Triangles,0);
+        mesh.UploadMeshData(false);
+        return mesh;
+    }
+
+    RenderTexture CreateRT(int size){
+        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(size, size, RenderTextureFormat.R8,0,1);
+        descriptor.autoGenerateMips = false;
+        descriptor.enableRandomWrite = true;
+        RenderTexture rt = new RenderTexture(descriptor);
+        rt.filterMode = FilterMode.Point;
+        rt.Create();
+        return rt;
+    }
+}
---
+++ Assets/Shaders/ShallowWater.shader
@@ -0,0 +1,63 @@
+Shader "John/ShallowWater"
+{
+    Properties
+    {
+    }
+
+    SubShader
+    {
+        LOD 0
+
+        Tags { "RenderPipeline" = "UniversalPipeline" "RenderType"="Opaque" "IgnoreProjector" = "True"}
+
+        Pass
+        {
+            Name "ForwardLit"
+    		Tags { "LightMode"="UniversalForward" }
+
+            Cull off
+
+            HLSLPROGRAM
+            #pragma target 3.0
+
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+            struct Attributes
+            {
+                float4 positionOS   : POSITION;
+                float2 uv           : TEXCOORD0;
+            };
+            struct Varyings
+            {
+                float4 positionCS   : SV_POSITION;
+                float2 uv           : TEXCOORD0;
+            };
+
+            TEXTURE2D(_H1RT);
+            SAMPLER(sampler_H1RT);
+
+            Varyings LitPassVertex(Attributes input)
+            {
+                Varyings output;
+
+                output.positionCS = TransformObjectToHClip(input.positionOS);
+                output.uv = input.uv;
+
+                return output;
+            }
+
+            half4 LitPassFragment(Varyings input) : SV_Target
+            {
+                half h = SAMPLE_TEXTURE2D(_H1RT, sampler_H1RT, input.uv);
+
+                return half4(h, h, h, 1);
+            }
+
+            #pragma vertex LitPassVertex
+            #pragma fragment LitPassFragment
+
+            ENDHLSL
+        }
+    }
+}
